857c4bdd72cceebab621bf9a11651dab75b4264b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-31 21:15:09 +0800

    fix(app): use correct default display for pagination

--- svelte/src/lib/components/datatable/Pagination.svelte
+++ svelte/src/lib/components/datatable/Pagination.svelte
@@ -14,54 +14,43 @@
 </script>
 
 <!-- Desktop buttons -->
-<section class="btn-group variant-ghost-surface [&>*+*]:border-surface-500 hidden h-10 lg:block">
+<section class="btn-group variant-ghost-surface hidden lg:inline-flex">
     <button
         type="button"
-        class="hover:variant-soft-primary"
+        class="hover:variant-soft-primary h-full"
         class:disabled={$pageNumber === 1}
-        on:click={() => handler.setPage('previous')}
+        on:click={() => handler.setPage('previous')}><Icon src={ArrowLeft} theme="micro" class="size-4" /></button
     >
-        <Icon src={ArrowLeft} theme="micro" class="size-4" />
-    </button>
     {#each $pages as page}
         <button
             type="button"
             class="hover:variant-soft-primary"
             class:active={$pageNumber === page}
             class:ellipse={page === null}
-            on:click={() => handler.setPage(page)}
+            on:click={() => handler.setPage(page)}>{page}</button
         >
-            {page}
-        </button>
     {:else}
         <div>...</div>
     {/each}
     <button
         type="button"
-        class="hover:variant-soft-primary"
+        class="hover:variant-soft-primary h-full"
         class:disabled={$pageNumber === $pageCount}
-        on:click={() => handler.setPage('next')}
+        on:click={() => handler.setPage('next')}><Icon src={ArrowRight} theme="micro" class="size-4" /></button
     >
-        <Icon src={ArrowRight} theme="micro" class="size-4" />
-    </button>
 </section>
-
 <!-- Mobile buttons -->
 <section class="lg:hidden">
     <button
         type="button"
-        class="btn variant-ghost-surface hover:variant-soft-primary mb-2 mr-2"
+        class="btn variant-ghost-surface hover:variant-soft-primary mb-2 mr-2 h-full"
         class:disabled={$pageNumber === 1}
-        on:click={() => handler.setPage('previous')}
+        on:click={() => handler.setPage('previous')}><Icon src={ArrowLeft} theme="micro" class="size-4" /></button
     >
-        <Icon src={ArrowLeft} theme="micro" class="size-4" />
-    </button>
     <button
         type="button"
-        class="btn variant-ghost-surface hover:variant-soft-primary mb-2"
+        class="btn variant-ghost-surface hover:variant-soft-primary mb-2 h-full"
         class:disabled={$pageNumber === $pageCount}
-        on:click={() => handler.setPage('next')}
+        on:click={() => handler.setPage('next')}><Icon src={ArrowRight} theme="micro" class="size-4" /></button
     >
-        <Icon src={ArrowRight} theme="micro" class="size-4" />
-    </button>
 </section>
