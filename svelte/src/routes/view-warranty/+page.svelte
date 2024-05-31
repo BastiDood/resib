249d1d3b5d1d6296b7e0b0b84a84a4e84d6022c0
Hans Lorico <hqlorico@up.edu.ph>
2024-05-31 18:17:08 +0800

    chore: transfer dependencies to devdependcies

--- svelte/src/routes/view-warranty/+page.svelte
+++ svelte/src/routes/view-warranty/+page.svelte
@@ -25,35 +25,41 @@
     Your Active Warranty Cards
     </h3>
 
-    <div class="w-full md:w-3/5 lg:w-4/5 card px-4 py-8 mb-4 bg-[#E5E5E5] grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center">
-        {#each data as item}
-            <div class="grid grid-cols-1 card card-hover p-4 sm:p-6 sm:max-w-96 justify-center variant-ghost-surface">
-                <div class="aspect-[3.370/2.125]">
-                    <div class="pb-4">
-                        <h3 class="h3 font-bold pb-10">
-                            WARRANTY
-                        </h3>
-                    </div>
-                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div>
-                            <h5 class="h5 text-xs opacity-40">Customer Name</h5>
-                            <p>{item.customer}</p>
+    <div class="md:w-3/5 lg:w-4/5 card px-4 py-8 mb-4 bg-[#E5E5E5]">
+        <div class="grid grid-cols-[repeat(auto-fill, minmax(16rem, 1fr))] gap-4 place-items-center">
+            {#each data as item}
+                <!-- <div class="grid  gap-4 justify-center  place-items-center aspect-[3.370/2.125]"> -->
+                    
+                <div class="card card-hover p-4 sm:p-6 variant-ghost-surface h-48">
+                    <div class="aspect-[3.370/2.125] w-64">
+                        <div class="pb-4">
+                            <h3 class="h3 font-bold pb-10">
+                                WARRANTY
+                            </h3>
                         </div>
-                        <div>
-                            <h5 class="h5 text-xs opacity-40">Product ID</h5>
-                            <p>{item.productId}</p>
-                        </div>
-                        <div>
-                            <h5 class="h5 text-xs opacity-40">Date Issued</h5>
-                            <p>{item.startDate}</p>
-                        </div>
-                        <div>
-                            <h5 class="h5 text-xs opacity-40">Valid Until</h5>
-                            <p>{item.endDate}</p>
-                        </div>
+                        <!-- <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                            <div>
+                                <h5 class="h5 text-xs opacity-40">Customer Name</h5>
+                                <p>{item.customer}</p>
+                            </div>
+                            <div>
+                                <h5 class="h5 text-xs opacity-40">Product ID</h5>
+                                <p>{item.productId}</p>
+                            </div>
+                            <div>
+                                <h5 class="h5 text-xs opacity-40">Date Issued</h5>
+                                <p>{item.startDate}</p>
+                            </div>
+                            <div>
+                                <h5 class="h5 text-xs opacity-40">Valid Until</h5>
+                                <p>{item.endDate}</p>
+                            </div>
+                        </div> -->
                     </div>
                 </div>
-            </div>
-        {/each}
+                    
+                <!-- </div> -->
+            {/each}
+        </div>
     </div>
 </div>
