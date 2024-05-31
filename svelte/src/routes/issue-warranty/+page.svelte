674fe26a6dbd4babbba673fd6b1d36eca0828b66
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-31 20:26:45 +0800

    refactor(app): prepare forms for warranty submission

--- svelte/src/routes/issue-warranty/+page.svelte
+++ svelte/src/routes/issue-warranty/+page.svelte
@@ -1,6 +1,22 @@
-<script>
+<script lang="ts">
     import Datatable from '$lib/components/datatable/Datatable.svelte';
     import Hero from '$lib/components/Hero.svelte';
+
+    function submit(form: HTMLFormElement, submitter: HTMLElement | null) {
+        if (submitter === null) return;
+        if (submitter instanceof HTMLButtonElement) {
+            const data = new FormData(form);
+            const id = data.get('product-id');
+            const name = data.get('customer-name');
+            submitter.disabled = true;
+            try {
+                // TODO: Invoke the smart contract here.
+                alert(`Submitted Product ${id} by ${name}.`);
+            } finally {
+                submitter.disabled = false;
+            }
+        }
+    }
 </script>
 
 <Hero>Create Warranty</Hero>
@@ -8,32 +24,36 @@
     <a href="/" class="btn variant-filled hover:underline">Go Back</a>
 </div>
 <div class="flex flex-col justify-center gap-4 md:flex-row">
-    <div class="card mb-4 flex w-full flex-col gap-2 bg-[#E5E5E5] p-4 md:w-2/5 md:gap-4 lg:w-1/5">
+    <form
+        method="post"
+        class="card mb-4 flex w-full flex-col gap-2 bg-[#E5E5E5] p-4 md:w-2/5 md:gap-4 lg:w-1/5"
+        on:submit|preventDefault|stopPropagation={({ currentTarget, submitter }) => submit(currentTarget, submitter)}
+    >
         <h3 class="h3 py-4 text-center font-bold">Create Warranty Card</h3>
         <label>
             <span>Product ID</span>
             <input
-                name="productId"
-                class="input variant-form-material"
-                placeholder="Enter Product ID"
                 type="number"
                 required
+                name="product-id"
+                placeholder="Enter Product ID"
+                class="input variant-form-material"
             />
         </label>
         <label>
             <span>Customer Name</span>
             <input
-                name="customer"
-                class="input variant-form-material"
-                placeholder="Enter Customer Name"
                 type="text"
                 required
+                name="customer-name"
+                placeholder="Enter Customer Name"
+                class="input variant-form-material"
             />
         </label>
         <div class="col-span-1 flex justify-center md:col-span-2">
             <button type="submit" class="btn variant-filled mt-5 text-white">Create Warranty</button>
         </div>
-    </div>
+    </form>
     <div class="card mb-4 w-full bg-[#E5E5E5] p-4 md:w-3/5 lg:w-3/5">
         <Datatable />
     </div>
