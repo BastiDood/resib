09367c660a94ad1b663c60f690801736e39f0604
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-08 21:39:11 +0800

    refactor(sol): overhaul architecture of contract storage

--- hardhat/contracts/Resib.sol
+++ hardhat/contracts/Resib.sol
@@ -3,69 +3,67 @@ pragma solidity ^0.8.0;
 
 contract Resib {
     struct Store {
-        uint id;
         string name;
         address owner;
+        Product[] products;
     }
 
     struct Product {
-        uint id;
         string name;
-        uint storeId;
         uint warrantyPeriod; // in days
+        Warranty[] warranties;
     }
 
     struct Warranty {
-        uint id;
-        uint productId;
         address customer;
         uint startDate;
         uint endDate;
     }
 
-    event StoreCreated(uint indexed id);
-    event ProductCreated(uint indexed id);
-    event WarrantyCreated(uint indexed id);
+    struct Index {
+        uint store;
+        uint product;
+        uint warranty;
+    }
 
-    Store[] public stores;
-    Product[] public products;
-    Warranty[] public warranties;
+    event StoreCreated(uint indexed store);
+    event ProductCreated(uint indexed store, uint indexed product);
+    event WarrantyCreated(uint indexed store, uint indexed product, uint indexed warranty);
 
-    // Create a store
+    Store[] _stores;
+    mapping(address => Index[]) _users;
+
     function createStore(string memory _name) public {
-        emit StoreCreated(stores.length);
-        stores.push(Store(stores.length, _name, msg.sender));
+        emit StoreCreated(_stores.length);
+        Product[] memory _init;
+        _stores.push(Store(_name, msg.sender, _init));
     }
 
-    // Read a store
-    function getStore(uint _storeId) public view returns (Store memory) {
-        require(stores[_storeId].owner == msg.sender, 'only the store owner can view info');
-        return stores[_storeId];
+    function getStores() public view returns (Store[] memory) {
+        return _stores;
     }
 
-    // Create a product
     function createProduct(uint _storeId, string memory _name, uint _warrantyPeriod) public {
-        require(stores[_storeId].owner == msg.sender, 'only the store owner can add products');
-        emit ProductCreated(products.length);
-        products.push(Product(products.length, _name, _storeId, _warrantyPeriod));
+        Store storage _store = _stores[_storeId];
+        require(_store.owner == msg.sender, 'only the store owner can add products');
+        Product[] storage _products = _store.products;
+
+        emit ProductCreated(_storeId, _products.length);
+        Warranty[] memory _warranties;
+        _products.push(Product(_name, _warrantyPeriod, _warranties));
     }
 
-    // Read a product
-    function getProduct(uint _productId) public view returns (Product memory) {
-        return products[_productId];
-    }
+    function createWarranty(uint _storeId, uint _productId, address _customer) public {
+        Store storage _store = _stores[_storeId];
+        require(_store.owner == msg.sender, 'only the store owner can issue warranties');
 
-    // Create a warranty
-    function createWarranty(uint _productId, address _customer) public {
-        require(stores[products[_productId].storeId].owner == msg.sender, 'only the store owner can issue warranties');
-        uint startDate = block.timestamp;
-        uint endDate = startDate + (products[_productId].warrantyPeriod * 1 days);
-        emit WarrantyCreated(warranties.length);
-        warranties.push(Warranty(warranties.length, _productId, _customer, startDate, endDate));
-    }
+        Product storage _product = _store.products[_productId];
+        Warranty[] storage _warranties = _product.warranties;
 
-    // Read a warranty
-    function getWarranty(uint _warrantyId) public view returns (Warranty memory) {
-        return warranties[_warrantyId];
+        uint _startDate = block.timestamp;
+        uint _endDate = _startDate + _product.warrantyPeriod * 1 days;
+
+        emit WarrantyCreated(_storeId, _productId, _warranties.length);
+        _warranties.push(Warranty(_customer, _startDate, _endDate));
     }
 }
