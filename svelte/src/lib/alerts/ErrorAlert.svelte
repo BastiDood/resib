26bae2814e2d8860f5e44f0b6b037927f56fc7d4
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-09 21:35:54 +0800

    feat(app): add basic alert components

---
+++ svelte/src/lib/alerts/ErrorAlert.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { ExclamationCircle } from '@steeze-ui/heroicons';
+    import { Icon } from '@steeze-ui/svelte-icon';
+</script>
+
+<div class="alert variant-soft-error flex flex-row items-center gap-x-2">
+    <Icon src={ExclamationCircle} class="size-8" />
+    <div class="!mt-0 contents">
+        <slot />
+    </div>
+</div>
---
+++ svelte/src/lib/alerts/WarningAlert.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { ExclamationTriangle } from '@steeze-ui/heroicons';
+    import { Icon } from '@steeze-ui/svelte-icon';
+</script>
+
+<div class="alert variant-soft-warning flex flex-row items-center gap-x-2">
+    <Icon src={ExclamationTriangle} class="size-8" />
+    <div class="!mt-0 contents">
+        <slot />
+    </div>
+</div>
