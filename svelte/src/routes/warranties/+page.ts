5726eee12f70a468a04bcdd69661198eebe4da27
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-10 02:27:13 +0800

    fix(app): wait for confirmations when creating stores

--- svelte/src/routes/warranties/+page.ts
+++ svelte/src/routes/warranties/+page.ts
@@ -3,8 +3,7 @@ import { resib } from '$lib/resib';
 
 async function getOwnedWarranties(parent: PageLoadEvent['parent']) {
     const { signer } = await parent();
-    if (signer === null) return [];
-    return await resib.getOwnedWarranties(signer.address);
+    return signer === null ? [] : await resib.getOwnedWarranties(signer.address);
 }
 
 export async function load({ parent }) {
